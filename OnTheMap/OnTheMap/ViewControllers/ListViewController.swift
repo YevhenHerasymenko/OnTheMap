c2e44481b448218fc5c00f869f2f8e27491cb7bc
Yevhen Herasymenko <fess2036@ya.ru>
2015-11-21 13:46:57 +0200

    feat: add controllers for tabBar structure

---
+++ OnTheMap/OnTheMap/ViewControllers/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewController.swift
+//  OnTheMap
+//
+//  Created by Yevhen Herasymenko on 21/11/2015.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ OnTheMap/OnTheMap/ViewControllers/PinTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  PinTabBarController.swift
+//  OnTheMap
+//
+//  Created by Yevhen Herasymenko on 21/11/2015.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+import UIKit
+
+class PinTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
